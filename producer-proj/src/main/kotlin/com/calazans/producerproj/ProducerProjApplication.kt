c29c5aafd827b8c104d7c8101b4ebcb808019395
luiz.calazans <luiz.calazans@movile.com>
2022-10-03 21:04:18 -0300

    producer project setup

---
+++ producer-proj/src/main/kotlin/com/calazans/producerproj/ProducerProjApplication.kt
@@ -0,0 +1,11 @@
+package com.calazans.producerproj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProducerProjApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProducerProjApplication>(*args)
+}
---
+++ producer-proj/src/test/kotlin/com/calazans/producerproj/ProducerProjApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.calazans.producerproj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProducerProjApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
